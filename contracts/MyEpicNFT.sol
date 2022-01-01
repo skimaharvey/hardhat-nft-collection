de669ff730a02f61748f9038a6b2297f5b8ba32c
ski <ski@skis-MacBook-Pro.local>
2022-01-01 17:10:43 +0300

    finished set up solidity contract. setting up front end with react

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.0;
 // We first import some OpenZeppelin Contracts.
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "hardhat/console.sol";
+
+import {Base64} from "./libraries/Base64.sol";
 
 // We inherit the contract we imported. This means we'll have access
 // to the inherited contract's methods.
@@ -12,9 +15,108 @@ contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    string[] firstWords = [
+        "ROBERT",
+        "JACK",
+        "MARLON",
+        "DENZEL",
+        "DANIEL",
+        "SIDNEY",
+        "CLARK",
+        "INGRID",
+        "TOM",
+        "GREGORY",
+        "LEONARDO",
+        "AUDREY",
+        "SPENCER",
+        "KATE",
+        "MARILYN"
+    ];
+
+    string[] secondWords = [
+        "BITTER",
+        "AMUSED",
+        "ANNOYING",
+        "AGREEABLE",
+        "ASHAMED",
+        "AWFUL",
+        "BETTER",
+        "BRAVE",
+        "BUSY",
+        "ANXIOUS",
+        "ANGRY",
+        "ADVENTUROUS",
+        "ATTRACTIVE",
+        "AVERAGE",
+        "BRAINY"
+    ];
+
+    string[] thirdWords = [
+        "AVOCADO",
+        "APPLE",
+        "APRICOT",
+        "BANANA",
+        "BLUEBERRY",
+        "COCONUT",
+        "GRAPEFRUIT",
+        "LEMON",
+        "MANGO",
+        "ORANGE",
+        "PEACH",
+        "PEAR",
+        "PINEAPPLE",
+        "POMEGRANATE",
+        "STRAWBERRY"
+    ];
+
+    string baseSvg =
+        "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
     // We need to pass the name of our NFTs token and its symbol.
     constructor() ERC721("SquareNFT", "SQUARE") {
         console.log("This is my NFT contract. Woah!");
+    }
+
+    function pickRandomFirstWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        // I seed the random generator. More on this in the lesson.
+        uint256 rand = random(
+            string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId)))
+        );
+        // Squash the # between 0 and the length of the array to avoid going out of bounds.
+        rand = rand % firstWords.length;
+        return firstWords[rand];
+    }
+
+    function pickRandomSecondWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("SECOND_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % secondWords.length;
+        return secondWords[rand];
+    }
+
+    function pickRandomThirdWord(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("THIRD_WORD", Strings.toString(tokenId)))
+        );
+        rand = rand % thirdWords.length;
+        return thirdWords[rand];
+    }
+
+    function random(string memory input) internal pure returns (uint256) {
+        return uint256(keccak256(abi.encodePacked(input)));
     }
 
     // A function our user will hit to get their NFT.
@@ -22,18 +124,58 @@ contract MyEpicNFT is ERC721URIStorage {
         // Get the current tokenId, this starts at 0.
         uint256 newItemId = _tokenIds.current();
 
+        string memory first = pickRandomFirstWord(newItemId);
+        string memory second = pickRandomSecondWord(newItemId);
+        string memory third = pickRandomThirdWord(newItemId);
+        string memory combinedWord = string(
+            abi.encodePacked(first, second, third)
+        );
+
+        string memory finalSvg = string(
+            abi.encodePacked(baseSvg, combinedWord, "</text></svg>")
+        );
+
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        // We set the title of our NFT as the generated word.
+                        combinedWord,
+                        '", "description": "A highly acclaimed collection of squares.", "image": "data:image/svg+xml;base64,',
+                        // We add data:image/svg+xml;base64 and then append our base64 encode our svg.
+                        Base64.encode(bytes(finalSvg)),
+                        '"}'
+                    )
+                )
+            )
+        );
+
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(
+            string(
+                abi.encodePacked(
+                    "https://nftpreview.0xdev.codes/?code=",
+                    finalTokenUri
+                )
+            )
+        );
+        console.log("--------------------\n");
+
         // Actually mint the NFT to the sender using msg.sender.
         _safeMint(msg.sender, newItemId);
 
         // Set the NFTs data.
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/R8T3");
+        _setTokenURI(newItemId, finalTokenUri);
         console.log(
             "An NFT w/ ID %s has been minted to %s",
             newItemId,
             msg.sender
         );
-
-        // Increment the counter for when the next NFT is minted.
         _tokenIds.increment();
     }
 }
